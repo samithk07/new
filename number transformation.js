398314ff72af59b8e46123008222b2072dbec3a8
samithk07 <samithsamith677@gmail.com>
2025-10-23 19:03:00 +0530

    new

---
+++ number transformation.js
@@ -0,0 +1,25 @@
+
+                                      // NUMBER TRANSFORMATION
+
+
+// // You are given an array of numbers:
+// // const numbers = [2, 5, 8, 12];
+// // 1.Write a function that returns the square of a number.
+//  2.Use this function to create a new array of squared numbers.
+//  3.Create a new array containing only the squared numbers that are greater than 50.
+// 4.Calculate the sum of all squared numbers.
+
+
+let num=[2,5,8,12]
+let square=num.map(num=>num**2)
+console.log(square)
+
+let greater=square.filter((a)=>{
+    return a>50
+})
+console.log(greater)
+let sum=square.reduce((a,b)=>a+b)
+console.log(sum)
+ 
+
+
